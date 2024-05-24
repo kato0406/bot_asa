8af46be07b2f46151afe36fe4e222332c07c3433
kato0406 <kato@valeur3.com>
2024-05-24 13:37:15 +0900

    リファクタ

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 const bot = {
   methods: {
-    isMonday() {
-      return dayjs.dayjs().day() === 1
-    },
     getLastRaw(sheet, column) {
       return sheet.getRange(sheet.getMaxRows(), column).getNextDataCell(SpreadsheetApp.Direction.UP).getRow()
     },
@@ -27,45 +24,6 @@ const bot = {
       spreadsheet.getSheetByName('プロジェクト一覧').getRange(`A2:B${projects.length + 1}`).setValues(projects)
       spreadsheet.getSheetByName('状態一覧').getRange(`A2:D${statuses.length + 1}`).setValues(statuses)
     },
-    generateBaseBacklogUrl() {
-      const spreadsheet = SpreadsheetApp.getActiveSpreadsheet()
-      const urlSheet = spreadsheet.getSheetByName('URL')
-      const statusValues = urlSheet.getRange(`E2:G${bot.methods.getLastRaw(urlSheet, 5)}`)
-      const projectValues = urlSheet.getRange(`A2:C${bot.methods.getLastRaw(urlSheet, 1)}`)
-      const statusIds = []
-      const projectIds = []
-      const completeStatusIds = []
-
-      statusValues.getValues()
-        .forEach(status => {
-          const [id, name, isActive] = status
-          if(!isActive) return
-
-          statusIds.push(id)
-      })
-      projectValues.getValues()
-        .forEach(project => {
-          const [id, name, isActive] = project
-          if(!isActive) return
-
-          projectIds.push(id)
-      })
-      statusValues.getValues()
-        .forEach(status => {
-          const [id, name, isActive] = status
-          if(isActive) return
-
-          completeStatusIds.push(id)
-      })
-      
-      return {
-        url: `${service.getProperty('BACKLOG_URL')}${projectIds.map(id => `projectId=${id}&`).join('')}${statusIds.map(id => `statusId=${id}&`).join('')}`,
-        mondayUrl: `${service.getProperty('BACKLOG_URL')}${projectIds.map(id => `projectId=${id}&`).join('')}${completeStatusIds.map(id => `statusId=${id}&`).join('')}`
-      }
-    },
-    formatDate(date) {
-      return date.format('YYYY/MM/DD')
-    },
     generateBody() {
       const spreadsheet = SpreadsheetApp.getActiveSpreadsheet()
       const sheet = spreadsheet.getSheetByName('基本設定')
@@ -85,11 +43,12 @@ const bot = {
       const workDays = [1, 2, 3, 4, 5]
       if (workDays.includes(date.day())) return date
 
-      return bot.methods.addDay(date.add(1, 'd'))
+      return bot.methods.skipHoliday(date.add(1, 'd'))
     },
     setTrigger() {
       const triggers = ScriptApp.getProjectTriggers()
-      const date = new Date(bot.methods.skipHoliday(dayjs.dayjs()).add(1, 'd'))
+      const date = new Date(bot.methods.skipHoliday(dayjs.dayjs().add(1, 'd')))
+     
       date.setHours(10, 10, 0, 0);
 
       for (const trigger in triggers) {
